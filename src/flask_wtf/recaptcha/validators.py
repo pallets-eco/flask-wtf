56fa5c4860353c8fd28d0b256cf97e1ddb60a10b
OmeirP <op6666667@gmail.com>
2025-10-26 14:05:21 +0000

    Added that recaptcha field automatically passes validation if app in testing mode to class-level docstring

--- src/flask_wtf/recaptcha/validators.py
+++ src/flask_wtf/recaptcha/validators.py
@@ -19,7 +19,7 @@ __all__ = ["Recaptcha"]
 
 
 class Recaptcha:
-    """Validates a ReCaptcha."""
+    """Validates a ReCaptcha. The field will always be valid if current_app.testing is True"""
 
     def __init__(self, message=None):
         if message is None:
